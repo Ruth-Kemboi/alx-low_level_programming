566de876ffda6a3a8d433371e8e11697a0b679a7
Ruth-Kemboi <ruthchirry254@gmail.com>
2022-03-12 06:48:20 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -18,13 +18,14 @@ int main(void)
 
 	for(i = 'a' ; i <= 'z' ; i++)
 	{
-		putchar(i);
+		putchar (i);
 	}
 	for(j = 'A' ; j <= 'Z' ; j++)
 	{
-		putchar(j);
+		putchar (j);
 	}
-	putchar('\n');
+	putchar ('\n');
+
 	return (0);
 }
 
